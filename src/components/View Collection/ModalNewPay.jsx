ed317ccfbd9f434894d9896511aa2954ce8def4d
TinoMaster <oscarmarcos2673@gmail.com>
2023-09-08 19:47:31 +0200

    ok

--- src/components/View Collection/ModalNewPay.jsx
+++ src/components/View Collection/ModalNewPay.jsx
@@ -17,8 +17,8 @@ export const ModalNewPay = (props) => {
         className={`w-10/12 relative bg-gradient-to-t ${
           darkMode
             ? "from-slate-800 to-darkMode"
-            : "from-slate-100 to-slate-200"
-        } p-3  rounded-md flex flex-col items-center shadow-lg font-roboto shadow-black/40`}
+            : "from-slate-100 to-slate-200 text-slate-600"
+        } p-4  rounded-md flex flex-col shadow-lg font-roboto shadow-black/40`}
       >
         {/* Error */}
         {error.error ? (
@@ -26,45 +26,39 @@ export const ModalNewPay = (props) => {
             <MdOutlineReportGmailerrorred className="text-xl" /> {error.message}
           </p>
         ) : null}
-        <h3 className="text-center py-3 text-xl font-bold">Nuevo pago</h3>
-        <div className="flex flex-col gap-2 items-center py-2">
-          <p>cantidad:</p>
+        <h3 className="py-3 text-2xl font-bold">Nuevo pago</h3>
+        <div className="">
+          <p className="pl-1 font-bold">cantidad</p>
           <input
             onChange={handleInputNewPay}
-            className="rounded-md bg-white/5 py-1 shadow-inner shadow-black/30 text-center focus:outline-none"
+            className={`w-full rounded-md p-2 shadow-inner ${
+              darkMode ? "bg-white/10" : "bg-white"
+            } shadow-black/30 focus:outline-none`}
             type="number"
             name="cantidad"
             value={newPay.cantidad}
           />
         </div>
-        <div className="w-full flex flex-col items-center py-2">
-          <p className="">comentario:</p>
+        <div className="w-full py-2">
+          <p className="pl-1 font-bold">comentario</p>
           <textarea
             onChange={handleInputNewPay}
-            className="overflow-auto resize-none w-full h-24 rounded-md shadow-inner shadow-black/30 bg-white/5 p-1 focus:outline-none"
+            className="overflow-auto resize-none w-full h-24 rounded-md shadow-inner shadow-black/30 p-2 focus:outline-none"
             name="comentario"
             value={newPay.comentario}
           />
         </div>
         {/* Buttons */}
-        <div className="flex justify-center gap-2 py-2">
+        <div className="flex justify-between gap-2 py-2 px-1">
           <button
             onClick={() => sendNewPay(id)}
-            className={`py-1 px-2 bg-gradient-to-tr ${
-              darkMode
-                ? "from-darkMode to-slate-700"
-                : "from-slate-200 to-slate-300"
-            }  rounded-md shadow-md`}
+            className={`w-1/2 p-2 bg-gradient-to-tr from-secondary to-secondary/50 text-white rounded-md shadow`}
           >
             Aceptar
           </button>
           <button
             onClick={closeModalNewPay}
-            className={`py-1 px-2 bg-gradient-to-tr ${
-              darkMode
-                ? "from-darkMode to-slate-700"
-                : "from-slate-200 to-slate-300"
-            }  rounded-md shadow-md`}
+            className={`w-1/2 p-2 bg-gradient-to-tr from-red-500 to-red-500/50 text-white  rounded-md shadow`}
           >
             Cancelar
           </button>
--- src/components/View Collection/ModalViewCard.jsx
+++ src/components/View Collection/ModalViewCard.jsx
@@ -4,11 +4,16 @@ import { FaDollarSign } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
 import { useModalViewCard } from "../../hooks/View Collection/useModalViewCard";
 import { ModalNewPay } from "./ModalNewPay";
-import { BsCalendar3, BsCashCoin, BsCheckAll } from "react-icons/bs";
+import {
+  BsCalendar3,
+  BsCashCoin,
+  BsCheckAll,
+  BsFillDoorClosedFill,
+} from "react-icons/bs";
 import { GiCash } from "react-icons/gi";
 import { ModalViewPay } from "./ModalViewPay";
 import { ModalPortal } from "../modals/modalPortal";
-import { MdPaid } from "react-icons/md";
+import { MdDelete, MdPaid } from "react-icons/md";
 
 export const ModalViewCard = ({
   debt,
@@ -81,7 +86,13 @@ export const ModalViewCard = ({
       {/* Card */}
       <div className="h-full flex flex-col relative p-2 gap-2 bg-black/5 rounded-md shadow-black/50">
         {/* Deuda total y resto */}
-        <div className="w-full p-2 bg-gradient-to-tr from-white to-white/90 rounded-md">
+        <div
+          className={`w-full p-2 bg-gradient-to-tr ${
+            darkMode
+              ? "from-white/5 to-white/10 border border-primary/80"
+              : "from-white to-white/90"
+          }  rounded-md`}
+        >
           <div style={{ color }} className="flex w-full gap-2 justify-between">
             <p className="flex gap-1 items-center">
               <GiCash />
@@ -117,7 +128,13 @@ export const ModalViewCard = ({
         </div>
         {/* Description and information */}
         <h3 className="w-full text-center text-sm text-slate-500">Info</h3>
-        <div className="w-full flex flex-wrap p-2 bg-gradient-to-tr text-slate-600 from-white to-white/90 rounded-md">
+        <div
+          className={`w-full flex flex-wrap p-2 bg-gradient-to-tr ${
+            darkMode
+              ? "from-white/5 to-white/10 border border-secondary/80"
+              : "from-white to-white/90"
+          }  rounded-md`}
+        >
           {/* fecha */}
           <div className="w-1/2 flex gap-1 p-1 justify-center items-center">
             <p className="font-semibold">Fecha:</p>
@@ -148,12 +165,22 @@ export const ModalViewCard = ({
         </div>
         {/* Pagos */}
         <h3 className="w-full text-center text-sm text-slate-500">Pagos</h3>
-        <div className="w-full h-full bg-white max-h-56 gap-1 flex flex-col p-2 pb-8 overflow-auto shadow-inner shadow-black/50 rounded-md">
+        <div
+          className={`w-full h-full bg-gradient-to-tr ${
+            darkMode
+              ? "from-white/5 to-white/10 border border-primary/80"
+              : "from-white/30 to-white/90"
+          } max-h-56 gap-1 flex flex-col p-2 pb-8 overflow-auto shadow-inner shadow-black/50 rounded-md`}
+        >
           {debt?.pagos.map((el) => (
             <div
               onClick={() => openModalViewPay(el, debt._id)}
               key={el?._id}
-              className="flex justify-between p-3 rounded-md text-sm shadow-md bg-white/90 hover:shadow-primary hover:cursor-pointer transition-all hover:bg-primary/5"
+              className={`flex justify-between p-3 rounded-md text-sm shadow-md bg-gradient-to-tr ${
+                darkMode
+                  ? "from-white/5 to-white/10 border border-primary/80"
+                  : "from-white to-white/90"
+              } hover:shadow-primary hover:cursor-pointer transition-all hover:bg-primary/5`}
             >
               <p className="text-sm flex gap-1 items-center">
                 <BsCalendar3 className="shadow-md text-primary bg-lightMode" />{" "}
@@ -167,7 +194,7 @@ export const ModalViewCard = ({
 
           <button
             onClick={openModalNewPay}
-            className={`absolute bottom-20 right-8 bg-gradient-to-tr ${
+            className={`absolute bottom-20 right-5 bg-gradient-to-tr ${
               darkMode
                 ? "from-darkMode to-slate-700 text-white"
                 : "from-lightMode to-slate-400"
@@ -179,24 +206,16 @@ export const ModalViewCard = ({
         {/* Buttons */}
         <div className="w-full flex justify-center py-2 gap-2">
           <button
-            className={`p-2 rounded-md bg-gradient-to-tr ${
-              darkMode
-                ? "from-darkMode to-slate-700"
-                : "from-lightMode to-slate-300"
-            }  hover:to-slate-600 transition-colors shadow-md shadow-black/20`}
+            className={`flex items-center gap-1 p-2 rounded-md bg-gradient-to-tr from-secondary to-secondary/50  hover:to-slate-600 transition-colors shadow-md shadow-black/20`}
             onClick={() => setModalViewCard(false)}
           >
-            Cerrar
+            <BsFillDoorClosedFill /> Cerrar
           </button>
           <button
-            className={`p-2 rounded-md bg-gradient-to-tr ${
-              darkMode
-                ? "from-darkMode to-slate-700"
-                : "from-lightMode to-slate-300"
-            }  hover:to-slate-600 transition-colors shadow-md shadow-black/20`}
+            className={`flex items-center gap-1 p-2 rounded-md bg-gradient-to-tr from-red-500 to-red-500/50  hover:to-slate-600 transition-colors shadow-md shadow-black/20`}
             onClick={(e) => deleteDebt(e, debt._id, countCard, navigate)}
           >
-            Borrar
+            <MdDelete /> Borrar
           </button>
         </div>
       </div>
